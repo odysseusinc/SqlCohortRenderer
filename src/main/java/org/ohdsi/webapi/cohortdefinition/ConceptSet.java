e75532d62bf2decce8a89990869906977b146ec5
Vitaly Koulakov <vitaly.koulakov@firstlinesoftware.com>
2017-06-21 14:22:41 +0300

    ARACHNE-886 Move SqlCohortRenderer to separate package and make Atlas use it

---
+++ src/main/java/org/ohdsi/webapi/cohortdefinition/ConceptSet.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ohdsi.webapi.cohortdefinition;
+
+
+import org.ohdsi.webapi.vocabulary.ConceptSetExpression;
+
+/**
+ *
+ * @author cknoll1
+ */
+public class ConceptSet {
+  
+  public int id;
+  public String name;
+  public ConceptSetExpression expression;
+}
